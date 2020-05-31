38643ff93bf042c5e247acce9db72825af4fa0d4
QuangMinh <quangminh1231998@gmail.com>
2020-05-31 15:17:26 +0700

    Remove the now ignored directory node_modules and move jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from '../src/containers/App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
